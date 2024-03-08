bf6119deb15208500bb87ade5365f4daa5229dbf
MrC08 <88736768+MrC08@users.noreply.github.com>
2024-03-08 13:56:49 -0600

    MCreator Merge commit '22755365c1d2273acaa97fe7af63908f23ec40c1' into 080ceba75051a425f848d4d8dd17d04ad9eb82dc

--- src/main/java/net/essorant/astraldisturbance/init/AstralDisturbanceModItems.java
+++ src/main/java/net/essorant/astraldisturbance/init/AstralDisturbanceModItems.java
@@ -20,6 +20,10 @@ public class AstralDisturbanceModItems {
 	public static final RegistryObject<Item> FLOATER_BAUBLE = REGISTRY.register("floater_bauble", () -> new FloaterBaubleItem());
 	public static final RegistryObject<Item> CRATER_SPREAD_BLOCK = block(AstralDisturbanceModBlocks.CRATER_SPREAD_BLOCK);
 	public static final RegistryObject<Item> GRAVITY_PLACER_BLOCK = block(AstralDisturbanceModBlocks.GRAVITY_PLACER_BLOCK);
+	public static final RegistryObject<Item> ASTRAL_STONE = block(AstralDisturbanceModBlocks.ASTRAL_STONE);
+	public static final RegistryObject<Item> ASTRAL_ORE_NATURAL = block(AstralDisturbanceModBlocks.ASTRAL_ORE_NATURAL);
+	public static final RegistryObject<Item> ASTRAL_ORE = block(AstralDisturbanceModBlocks.ASTRAL_ORE);
+	public static final RegistryObject<Item> BIOME_SPREAD_BLOCK = block(AstralDisturbanceModBlocks.BIOME_SPREAD_BLOCK);
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties()));
